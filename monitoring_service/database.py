7608f0011981101532f4e1a42434f0f908f7a66e
stavros <stavlir6@gmail.com>
2023-06-10 20:39:50 +0300

    Created monitoring. No fault tolerance for now

--- monitoring_service/database.py
+++ monitoring_service/database.py
@@ -1,12 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 db = SQLAlchemy()
 
 class Job(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.String(80), nullable=False)
-    user_id = db.Column(db.String(80), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(120), nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
+class Worker(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(120), nullable=False)
+    status = db.Column(db.String(80), nullable=False)
+
+def init_app(app):
+    db.init_app(app)
--- monitoring_service/monitor.py
+++ monitoring_service/monitor.py
@@ -1,15 +1,28 @@
 from flask import Flask, request, jsonify
-from database import db, Job, init_app
+from database import db, Job, Worker, init_app
+import grpc
+import worker_pb2
+import worker_pb2_grpc
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+stub_dict = {}
 
 @app.route('/job', methods=['POST'])
 def submit_job():
     data = request.get_json()
-    new_job = Job(status='submitted', user_id=data['user_id'], description=data['description'])
+    new_job = Job(status='submitted', user_id=data['user_id'], description=data['job_description'])
     db.session.add(new_job)
     db.session.commit()
+
+    for worker in Worker.query.all():
+        print(worker, worker.address)
+        stub = worker_pb2_grpc.WorkerStub(grpc.insecure_channel(worker.address))
+        task = worker_pb2.Task()
+        task.task_type = worker_pb2.Task.TaskType.Value(data['task_type'])
+        task.input_path = data['input_path']
+        task.output_path = data['output_path']
+        response = stub.AssignTask(task)
 
     return jsonify({'message': 'New job created!', 'job_id': new_job.id})
 
@@ -18,8 +31,36 @@ def view_jobs():
     jobs = Job.query.all()
     return jsonify([{'job_id': job.id, 'status': job.status, 'user_id': job.user_id, 'description': job.description} for job in jobs])
 
+@app.route('/register', methods=['POST'])
+def register_worker():
+    data = request.get_json()
+    new_worker = Worker(address=data['address'], status='IDLE')
+    db.session.add(new_worker)
+    db.session.commit()
+
+    stub = worker_pb2_grpc.WorkerStub(grpc.insecure_channel(new_worker.address))
+    stub_dict[new_worker.id] = stub
+
+    return jsonify({'message': 'New worker registered!', 'worker_id': new_worker.id})
+
+@app.route('/deregister', methods=['POST'])
+def deregister_worker():
+    data = request.get_json()
+    worker = Worker.query.get(data['worker_id'])
+    db.session.delete(worker)
+    db.session.commit()
+
+    del stub_dict[worker.id]
+
+    return jsonify({'message': 'Worker deregistered!'})
+
+@app.route('/workers', methods=['GET'])
+def view_workers():
+    workers = Worker.query.all()
+    return jsonify([{'worker_id': worker.id, 'address': worker.address, 'status': worker.status} for worker in workers])
+
 if __name__ == '__main__':
     db.init_app(app)
     with app.app_context():
-        db.create_all()  
+        db.create_all()
     app.run(host='0.0.0.0', port=6000)
